5f9845973ba080a4791e0805e5701e185f57ea83
Colin <colin.rosati@finiata.com>
2020-05-05 17:28:48 +0200

    Update 0(N) solution using set

--- build/lesson-04/FrogRiverOne/solution.js
+++ build/lesson-04/FrogRiverOne/solution.js
@@ -5,63 +5,50 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports.solution = void 0;
 
+function _slicedToArray(arr, i) { return _arrayWithHoles(arr) || _iterableToArrayLimit(arr, i) || _unsupportedIterableToArray(arr, i) || _nonIterableRest(); }
+
+function _nonIterableRest() { throw new TypeError("Invalid attempt to destructure non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); }
+
+function _iterableToArrayLimit(arr, i) { if (typeof Symbol === "undefined" || !(Symbol.iterator in Object(arr))) return; var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"] != null) _i["return"](); } finally { if (_d) throw _e; } } return _arr; }
+
+function _arrayWithHoles(arr) { if (Array.isArray(arr)) return arr; }
+
+function _createForOfIteratorHelper(o) { if (typeof Symbol === "undefined" || o[Symbol.iterator] == null) { if (Array.isArray(o) || (o = _unsupportedIterableToArray(o))) { var i = 0; var F = function F() {}; return { s: F, n: function n() { if (i >= o.length) return { done: true }; return { done: false, value: o[i++] }; }, e: function e(_e2) { throw _e2; }, f: F }; } throw new TypeError("Invalid attempt to iterate non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); } var it, normalCompletion = true, didErr = false, err; return { s: function s() { it = o[Symbol.iterator](); }, n: function n() { var step = it.next(); normalCompletion = step.done; return step; }, e: function e(_e3) { didErr = true; err = _e3; }, f: function f() { try { if (!normalCompletion && it["return"] != null) it["return"](); } finally { if (didErr) throw err; } } }; }
+
+function _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o === "string") return _arrayLikeToArray(o, minLen); var n = Object.prototype.toString.call(o).slice(8, -1); if (n === "Object" && o.constructor) n = o.constructor.name; if (n === "Map" || n === "Set") return Array.from(n); if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen); }
+
+function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) { arr2[i] = arr[i]; } return arr2; }
+
 var solution = function solution(X, A) {
-  console.log(X, A);
-  linearArray(X);
-  var pathSteps = linearArray(X);
-  var positionPath = [];
-  var solution = -1;
+  var leafPositions = new Set();
+  var count = 0;
 
-  var matches = function matches(item) {
-    return positionPath.includes(item);
-  };
+  var _iterator = _createForOfIteratorHelper(A.entries()),
+      _step;
 
-  for (var i = 0; i < A.length; i++) {
-    positionPath.push(A[i]);
+  try {
+    for (_iterator.s(); !(_step = _iterator.n()).done;) {
+      var _step$value = _slicedToArray(_step.value, 2),
+          index = _step$value[0],
+          item = _step$value[1];
 
-    if (pathSteps.every(matches)) {
-      console.log("result", i);
-      solution = i;
-      return i;
+      leafPositions.add(item);
+
+      if (item !== undefined) {
+        console.log(leafPositions, item);
+
+        if (leafPositions.size === X) {
+          return index;
+        }
+      }
     }
+  } catch (err) {
+    _iterator.e(err);
+  } finally {
+    _iterator.f();
   }
 
-  return solution;
+  return -1;
 };
 
 exports.solution = solution;
-
-var linearArray = function linearArray(max) {
-  var _newArr = [];
-
-  for (var x = 1; x <= max; x++) {
-    _newArr.push(x);
-  }
-
-  return _newArr;
-}; // export const solution = (X, A) => {
-// 	console.log(X, A);
-// 	let jumpTime = 0;
-// 	const pathStep = [];
-// 	const positionPath = [];
-// 	A.forEach((item, index) => {
-// 		if (index + 1 <= X) {
-// 			positionPath.push(item);
-// 			pathStep.push(index + 1);
-// 			let count = index + 2;
-// 			jumpTime = count;
-// 		}
-// 	});
-// 	path.pop();
-// 	console.log("position", positionPath, "path", pathStep);
-// 	const find = (item) => positionPath.includes(item);
-// 	console.log("find", find);
-// 	console.log(pathStep.every(find));
-// 	console.log("A.indexOd(X)", A.indexOf(X));
-// 	if (pathStep.every(find)) {
-// 		console.log("solution", jumpTime);
-// 		return A.indexOf(X);
-// 	} else {
-// 		return -1;
-// 	}
-// };
--- src/lesson-04/FrogRiverOne/solution.js
+++ src/lesson-04/FrogRiverOne/solution.js
@@ -1,27 +1,13 @@
 export const solution = (X, A) => {
-	console.log(X, A);
-	linearArray(X);
-	let pathSteps = linearArray(X);
-	let positionPath = [];
-	let solution = -1;
-
-	const matches = (item) => positionPath.includes(item);
-
-	for (var i = 0; i < A.length; i++) {
-		positionPath.push(A[i]);
-		if (pathSteps.every(matches)) {
-			console.log("result", i);
-			solution = i;
-			return i;
+	let leafPositions = new Set();
+	let count = 0;
+	for (var [index, item] of A.entries()) {
+		leafPositions.add(item);
+		if (item !== undefined) {
+			if (leafPositions.size === X) {
+				return index;
+			}
 		}
 	}
-	return solution;
-};
-
-const linearArray = (max) => {
-	let _newArr = [];
-	for (var x = 1; x <= max; x++) {
-		_newArr.push(x);
-	}
-	return _newArr;
+	return -1;
 };
